847a03e52e1c8532ba8dd5c9cfee40f43579e9dd
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-10-05 23:13:22 +0200

    Added spring-boot config to web-layer

--- neuban/web/src/main/java/hu/unideb/inf/rft/neuban/web/configuration/Application.java
+++ neuban/web/src/main/java/hu/unideb/inf/rft/neuban/web/configuration/Application.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfiguration.class)
 public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-  
-  @Override
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(Application.class).web(true);
 	}
