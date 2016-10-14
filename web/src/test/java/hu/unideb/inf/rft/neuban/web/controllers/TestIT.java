54d42bdc70f8fa2774d360a77e452b3dce1fab8c
atka66 <atkavagyok@gmail.com>
2016-10-14 21:32:10 +0200

    Actual integration test profile

---
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/TestIT.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.rft.neuban.web.controllers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestIT {
+	@Test
+	public void testTest() {
+		Assert.assertTrue(true);
+	}
+}
