dcbb6d79254d55e0110698ce2674a5763643bad7
Erdei Krisztian <headswitcher@gmail.com>
2016-10-29 19:54:42 +0200

    +edit - 	pass@Size(min = 5, max = 20) usname@Size(min = 3, max = 20)

--- persistence/src/main/resources/data.sql
+++ persistence/src/main/resources/data.sql
@@ -1,76 +1,76 @@
 delete from user where id>0 and id < 26;
 
 insert into user (id,password,user_name)
-values (25,'wood','aberrant');
+values (25,'wood123','aberrant');
 
 insert into user (id,password,user_name)
-values (24,'cut','overrated');
+values (24,'cut24324234','overrated');
 
 insert into user (id,password,user_name)
-values (23,'ting','burdensome');
+values (23,'ting123123','burdensome');
 
 insert into user (id,password,user_name)
-values (22,'krisz','debonair');
+values (22,'kriszasdsad','debonair');
 
 insert into user (id,password,user_name)
-values (21,'tian','arid');
+values (21,'tianasdsad','aridasd');
 
 insert into user (id,password,user_name)
-values (20,'chopy','unequaled');
+values (20,'chopyasdsad','unequaled');
 
 insert into user (id,password,user_name)
-values (19,'chopi','linear');
+values (19,'chopiasd','linearasd');
 
 insert into user (id,password,user_name)
-values (18,'ztura','bewitched');
+values (18,'zturaasd','bewitchedasd');
 
 insert into user (id,password,user_name)
-values (17,'csak5','metallic');
+values (17,'csak5asd','metallicasd');
 
 insert into user (id,password,user_name)
-values (16,'57896','trained');
+values (16,'57896dsa','trained');
 
 insert into user (id,password,user_name)
-values (15,'ad13','fragile');
+values (15,'ad1asd3','fragile');
 
 insert into user (id,password,user_name)
-values (14,'4897','gleaming');
+values (14,'4897123as','gleaming');
 
 insert into user (id,password,user_name)
-values (13,'dsdfs','omniscient');
+values (13,'dsdfsasd','omniscient');
 
 insert into user (id,password,user_name)
-values (12,'123a','forked');
+values (12,'123aqweas','forked');
 
 insert into user (id,password,user_name)
-values (11,'6512','heady');
+values (11,'6512asd2','heady');
 
 insert into user (id,password,user_name)
-values (10,'54798','amuck');
+values (10,'54798asd2','amuck');
 
 insert into user (id,password,user_name)
-values (9,'9876','woodcutterkrisz');
+values (9,'9876ads2','woodcutterkrisz');
 
 insert into user (id,password,user_name)
-values (8,'12345','gigantic');
+values (8,'12345asdas','gigantic');
 
 insert into user (id,password,user_name)
-values (7,'gktlr','adolescent');
+values (7,'gktlrasdsad','adolescent');
 
 insert into user (id,password,user_name)
-values (6,'asdq','boisterous');
+values (6,'asdqda2sad','boisterous');
 
 insert into user (id,password,user_name)
-values (5,'12345','traumatic');
+values (5,'12345asdasd','traumatic');
 
 insert into user (id,password,user_name)
-values (4,'12as','chopchop');
+values (4,'12asasd1sa','chopchop');
 
 insert into user (id,password,user_name)
-values (3,'12ASD','krisztmas');
+values (3,'12ASD213wq3','krisztmas');
 
 insert into user (id,password,user_name)
-values (2,'12345','pivanyi');
+values (2,'12345yxcfxnw3','pivanyi');
 
 insert into user (id,password,user_name)
-values (1,'1231a','falatka');
+values (1,'1231ayxcaw3	','falatka');
