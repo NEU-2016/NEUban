8539082154a312d003a6d66f43fb969afca232e3
Erdei Krisztian <headswitcher@gmail.com>
2016-10-29 18:23:02 +0200

    Sql insert script for user table

---
+++ persistence/src/main/resources/data.sql
@@ -0,0 +1,74 @@
+insert into user (id,password,user_name)
+values (25,'wood','aberrant');
+
+insert into user (id,password,user_name)
+values (24,'cut','overrated');
+
+insert into user (id,password,user_name)
+values (23,'ting','burdensome');
+
+insert into user (id,password,user_name)
+values (22,'krisz','debonair');
+
+insert into user (id,password,user_name)
+values (21,'tian','arid');
+
+insert into user (id,password,user_name)
+values (20,'chopy','unequaled');
+
+insert into user (id,password,user_name)
+values (19,'chopi','linear');
+
+insert into user (id,password,user_name)
+values (18,'ztura','bewitched');
+
+insert into user (id,password,user_name)
+values (17,'csak5','metallic');
+
+insert into user (id,password,user_name)
+values (16,'57896','trained');
+
+insert into user (id,password,user_name)
+values (15,'ad13','fragile');
+
+insert into user (id,password,user_name)
+values (14,'4897','gleaming');
+
+insert into user (id,password,user_name)
+values (13,'dsdfs','omniscient');
+
+insert into user (id,password,user_name)
+values (12,'123a','forked');
+
+insert into user (id,password,user_name)
+values (11,'6512','heady');
+
+insert into user (id,password,user_name)
+values (10,'54798','amuck');
+
+insert into user (id,password,user_name)
+values (9,'9876','woodcutterkrisz');
+
+insert into user (id,password,user_name)
+values (8,'12345','gigantic');
+
+insert into user (id,password,user_name)
+values (7,'gktlr','adolescent');
+
+insert into user (id,password,user_name)
+values (6,'asdq','boisterous');
+
+insert into user (id,password,user_name)
+values (5,'12345','traumatic');
+
+insert into user (id,password,user_name)
+values (4,'12as','chopchop');
+
+insert into user (id,password,user_name)
+values (3,'12ASD','krisztmas');
+
+insert into user (id,password,user_name)
+values (2,'12345','pivanyi');
+
+insert into user (id,password,user_name)
+values (1,'1231a','falatka');
