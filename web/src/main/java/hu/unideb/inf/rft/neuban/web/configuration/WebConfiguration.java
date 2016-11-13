09a669fa78cf3fe27fbd64ed84cbc11ef486d8ee
Fekete Attila <atkavagyok@gmail.com>
2016-11-13 15:22:06 +0100

    Initial commit, moved i18n properties, configuration bean created

--- web/src/main/java/hu/unideb/inf/rft/neuban/web/configuration/WebConfiguration.java
+++ web/src/main/java/hu/unideb/inf/rft/neuban/web/configuration/WebConfiguration.java
@@ -3,7 +3,6 @@ package hu.unideb.inf.rft.neuban.web.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
 import hu.unideb.inf.rft.neuban.service.configuration.ServiceConfiguration;
 
 @Configuration
