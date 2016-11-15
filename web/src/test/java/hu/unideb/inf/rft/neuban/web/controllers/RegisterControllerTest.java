e73c7bb9b1fd033852678da3d1e7722dcffd3b29
Fekete Attila <atkavagyok@gmail.com>
2016-11-15 20:53:16 +0100

    RegisterControllerTest implemented, they still fail

--- web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
@@ -6,42 +6,93 @@ import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RegisterControllerTest extends AbstractControllerTest {
 
-    private static final String REQUEST_URL = "/register";
-    private static final String VIEW_NAME = "register";
-    private static final String INDEX_VIEW = "index";
+	private static final String REQUEST_URL = "/register";
+	private static final String VIEW_NAME = "register";
+	private static final String INDEX_VIEW = "index";
 
-    @InjectMocks
-    private RegisterController registerController;
+	private static final String USER_USERNAME_FIELD_NAME = "userName";
+	private static final String USER_PASSWORD_FIELD_NAME = "password";
+	private static final String USER_PASSWORDCONFIRMATION_FIELD_NAME = "passwordConfirmation";
 
-    @Override
-    protected Object[] controllerUnderTest() {
-        return new Object[]{this.registerController};
-    }
+	private static final String VALID_USERNAME = "username";
+	private static final String VALID_PASSWORD = "password";
+	private static final String VALID_PASSWORDCONFIRMATION = "password";
 
-    @Test
-    public void loadRegisterViewShouldRenderRegisterView() throws Exception {
-        this.mockMvc.perform(get(REQUEST_URL))
-                .andExpect(status().isOk())
-                .andExpect(view().name(VIEW_NAME))
-                .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX));
-    }
+	private static final String INVALID_USERNAME = "us";
+	private static final String INVALID_PASSWORD = "pas";
+	private static final String INVALID_PASSWORDCONFIRMATION = "pas";
 
-    //TODO Pass an invalid ModelAttribute, read out error from bindingResult, view should be INDEX_VIEW
-    /*
-    @Test
-    public void userRegisterShouldRenderRegisterViewWithErrorsIfUserAttributeIsValid() throws Exception {
-    }
-    */
+	@InjectMocks
+	private RegisterController registerController;
 
-    //TODO Pass a valid ModelAttribute, view should be VIEW_NAME
-    /*
-    @Test
-    public void userRegisterShouldRenderRegisterViewWithErrorsIfUserAttributeIsValid() throws Exception {
-    }
-    */
+	@Override
+	protected Object[] controllerUnderTest() {
+		return new Object[]{this.registerController};
+	}
+
+	@Test
+	public void loadRegisterViewShouldRenderRegisterView() throws Exception {
+		this.mockMvc.perform(get(REQUEST_URL))
+				.andExpect(status().isOk())
+				.andExpect(view().name(VIEW_NAME))
+				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX));
+	}
+
+	@Test
+	public void userRegisterShouldRenderIndexViewIfUserAttributeIsValid() throws Exception {
+		this.mockMvc.perform(post(REQUEST_URL)
+				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
+				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
+				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, VALID_PASSWORDCONFIRMATION)
+		)
+				.andExpect(status().isOk())
+				.andExpect(view().name(INDEX_VIEW))
+				.andExpect(forwardedUrl(VIEW_PREFIX + INDEX_VIEW + VIEW_SUFFIX))
+				.andExpect(model().hasNoErrors());
+	}
+
+	@Test
+	public void userRegisterShouldRenderRegisterViewWithUsernameErrorIfUserAttributeHasInvalidUsername() throws Exception {
+		this.mockMvc.perform(post(REQUEST_URL)
+				.param(USER_USERNAME_FIELD_NAME, INVALID_USERNAME)
+				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
+				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, VALID_PASSWORDCONFIRMATION)
+		)
+				.andExpect(status().isOk())
+				.andExpect(view().name(VIEW_NAME))
+				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
+				.andExpect(model().attributeHasErrors(USER_USERNAME_FIELD_NAME));
+	}
+
+	@Test
+	public void userRegisterShouldRenderRegisterViewWithPasswordErrorIfUserAttributeHasInvalidPassword() throws Exception {
+		this.mockMvc.perform(post(REQUEST_URL)
+				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
+				.param(USER_PASSWORD_FIELD_NAME, INVALID_PASSWORD)
+				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, VALID_PASSWORDCONFIRMATION)
+		)
+				.andExpect(status().isOk())
+				.andExpect(view().name(VIEW_NAME))
+				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
+				.andExpect(model().attributeHasErrors(USER_PASSWORD_FIELD_NAME));
+	}
+
+	@Test
+	public void userRegisterShouldRenderRegisterViewWithPasswordConfirmationErrorIfUserAttributeHasInvalidPasswordConfirmation() throws Exception {
+		this.mockMvc.perform(post(REQUEST_URL)
+				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
+				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
+				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, INVALID_PASSWORDCONFIRMATION)
+		)
+				.andExpect(status().isOk())
+				.andExpect(view().name(VIEW_NAME))
+				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
+				.andExpect(model().attributeHasErrors(USER_PASSWORDCONFIRMATION_FIELD_NAME));
+	}
 }
