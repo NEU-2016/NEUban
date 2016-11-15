cbac0ee2e897211fdf5dc3a2e8b376b0af1aa5aa
Fekete Attila <atkavagyok@gmail.com>
2016-11-15 19:41:25 +0100

    RegisterControllerTest needs to be implemented

--- web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
@@ -6,15 +6,14 @@ import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RegisterControllerTest extends AbstractControllerTest {
 
     private static final String REQUEST_URL = "/register";
     private static final String VIEW_NAME = "register";
+    private static final String INDEX_VIEW = "index";
 
     @InjectMocks
     private RegisterController registerController;
@@ -31,4 +30,18 @@ public class RegisterControllerTest extends AbstractControllerTest {
                 .andExpect(view().name(VIEW_NAME))
                 .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX));
     }
+
+    //TODO Pass an invalid ModelAttribute, read out error from bindingResult, view should be INDEX_VIEW
+    /*
+    @Test
+    public void userRegisterShouldRenderRegisterViewWithErrorsIfUserAttributeIsValid() throws Exception {
+    }
+    */
+
+    //TODO Pass a valid ModelAttribute, view should be VIEW_NAME
+    /*
+    @Test
+    public void userRegisterShouldRenderRegisterViewWithErrorsIfUserAttributeIsValid() throws Exception {
+    }
+    */
 }
