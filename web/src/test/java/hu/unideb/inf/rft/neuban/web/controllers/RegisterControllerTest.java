45199b1581e0756a968154a341faed00676d806f
Fekete Attila <atkavagyok@gmail.com>
2016-11-21 21:28:00 +0100

    Tests correctly implemented

--- web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/RegisterControllerTest.java
@@ -1,10 +1,21 @@
 package hu.unideb.inf.rft.neuban.web.controllers;
 
+import hu.unideb.inf.rft.neuban.service.domain.UserDto;
+import hu.unideb.inf.rft.neuban.service.interfaces.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.Validator;
 
+import java.util.Collections;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,6 +42,15 @@ public class RegisterControllerTest extends AbstractControllerTest {
 	@InjectMocks
 	private RegisterController registerController;
 
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private Validator validator;
+
+	@Mock
+	private Errors errors;
+
 	@Override
 	protected Object[] controllerUnderTest() {
 		return new Object[]{this.registerController};
@@ -46,6 +66,15 @@ public class RegisterControllerTest extends AbstractControllerTest {
 
 	@Test
 	public void userRegisterShouldRenderIndexViewIfUserAttributeIsValid() throws Exception {
+		doNothing().when(validator).validate(
+				UserDto
+						.builder()
+						.userName(VALID_USERNAME)
+						.password(VALID_PASSWORD)
+						.passwordConfirmation(VALID_PASSWORDCONFIRMATION)
+						.build(),
+				errors
+		);
 		this.mockMvc.perform(post(REQUEST_URL)
 				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
 				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
@@ -59,40 +88,48 @@ public class RegisterControllerTest extends AbstractControllerTest {
 
 	@Test
 	public void userRegisterShouldRenderRegisterViewWithUsernameErrorIfUserAttributeHasInvalidUsername() throws Exception {
-		this.mockMvc.perform(post(REQUEST_URL)
-				.param(USER_USERNAME_FIELD_NAME, INVALID_USERNAME)
-				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
-				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, VALID_PASSWORDCONFIRMATION)
-		)
-				.andExpect(status().isOk())
-				.andExpect(view().name(VIEW_NAME))
-				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
-				.andExpect(model().attributeHasErrors(USER_USERNAME_FIELD_NAME));
+		testWithIncorrectCredentialsAndErrors(INVALID_USERNAME, VALID_PASSWORD, VALID_PASSWORDCONFIRMATION, USER_USERNAME_FIELD_NAME);
 	}
 
 	@Test
 	public void userRegisterShouldRenderRegisterViewWithPasswordErrorIfUserAttributeHasInvalidPassword() throws Exception {
-		this.mockMvc.perform(post(REQUEST_URL)
-				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
-				.param(USER_PASSWORD_FIELD_NAME, INVALID_PASSWORD)
-				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, VALID_PASSWORDCONFIRMATION)
-		)
-				.andExpect(status().isOk())
-				.andExpect(view().name(VIEW_NAME))
-				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
-				.andExpect(model().attributeHasErrors(USER_PASSWORD_FIELD_NAME));
+		testWithIncorrectCredentialsAndErrors(VALID_USERNAME, INVALID_PASSWORD, VALID_PASSWORDCONFIRMATION, USER_PASSWORD_FIELD_NAME);
 	}
 
 	@Test
 	public void userRegisterShouldRenderRegisterViewWithPasswordConfirmationErrorIfUserAttributeHasInvalidPasswordConfirmation() throws Exception {
+		testWithIncorrectCredentialsAndErrors(VALID_USERNAME, VALID_PASSWORD, INVALID_PASSWORDCONFIRMATION, USER_PASSWORDCONFIRMATION_FIELD_NAME);
+	}
+
+	private void testWithIncorrectCredentialsAndErrors(
+			String username,
+			String password,
+			String passwordConfirmation,
+			String expectedErrorField
+	) throws Exception {
+		doNothing().when(validator).validate(
+				UserDto
+						.builder()
+						.userName(username)
+						.password(password)
+						.passwordConfirmation(passwordConfirmation)
+						.build(),
+				errors
+		);
+
+		FieldError fieldError = mock(FieldError.class);
+
+		given(errors.getAllErrors()).willReturn(Collections.singletonList(fieldError));
+		given(fieldError.getField()).willReturn(expectedErrorField);
+
 		this.mockMvc.perform(post(REQUEST_URL)
-				.param(USER_USERNAME_FIELD_NAME, VALID_USERNAME)
-				.param(USER_PASSWORD_FIELD_NAME, VALID_PASSWORD)
-				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, INVALID_PASSWORDCONFIRMATION)
+				.param(USER_USERNAME_FIELD_NAME, username)
+				.param(USER_PASSWORD_FIELD_NAME, password)
+				.param(USER_PASSWORDCONFIRMATION_FIELD_NAME, passwordConfirmation)
 		)
 				.andExpect(status().isOk())
 				.andExpect(view().name(VIEW_NAME))
 				.andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX))
-				.andExpect(model().attributeHasErrors(USER_PASSWORDCONFIRMATION_FIELD_NAME));
+				.andExpect(model().attributeExists(expectedErrorField));
 	}
 }
