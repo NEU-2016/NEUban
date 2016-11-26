10cad801738b8be92aa242aac1e66fbed151a201
Fekete Attila <atkavagyok@gmail.com>
2016-11-26 21:04:14 +0100

    welcome view will show a summary of boards of the user, board view is the detail page of the chosen board, logout now redirects you to the index page

--- web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/BoardController.java
+++ web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/BoardController.java
@@ -1,27 +1,27 @@
 package hu.unideb.inf.rft.neuban.web.controllers;
 
+import hu.unideb.inf.rft.neuban.service.interfaces.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path = "/secure/boards")
+@RequestMapping(path = "/secure/board")
 public class BoardController {
 
-	private static final String BOARDS_VIEW = "secure/boards";
+	private static final String BOARD_VIEW = "secure/board";
 
-	@GetMapping
-	public String loadBoardsView() {
-		return BOARDS_VIEW;
-	}
+	@Autowired
+	private BoardService boardService;
 
-	@PostMapping(path = "/create")
-	public ModelAndView createBoard(String username, String boardTitle) {
+	@GetMapping(path = "/{boardId}")
+	public ModelAndView loadBoardView(@PathVariable Long boardId) {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(BOARDS_VIEW);
-		//TODO implementing board creation
+		modelAndView.setViewName(BOARD_VIEW);
+		modelAndView.addObject("board", boardService.get(boardId).orElse(null));
 		return modelAndView;
 	}
 }
--- web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/WelcomeController.java
+++ web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/WelcomeController.java
@@ -1,17 +1,59 @@
 package hu.unideb.inf.rft.neuban.web.controllers;
 
+import hu.unideb.inf.rft.neuban.service.domain.UserDto;
+import hu.unideb.inf.rft.neuban.service.exceptions.NonExistentUserIdException;
+import hu.unideb.inf.rft.neuban.service.handler.BoardHandler;
+import hu.unideb.inf.rft.neuban.service.interfaces.BoardService;
+import hu.unideb.inf.rft.neuban.service.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/secure/welcome")
 public class WelcomeController {
 
 	private static final String WELCOME_VIEW = "secure/welcome";
+	private static final String REDIRECT_URL_TO_WELCOME_VIEW = "redirect:/" + WELCOME_VIEW;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private BoardService boardService;
+
+	@Autowired
+	private BoardHandler boardHandler;
 
 	@GetMapping
-	public String loadWelcomeView() {
-		return WELCOME_VIEW;
+	public ModelAndView loadWelcomeView() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName(WELCOME_VIEW);
+		UserDto currentUser = userService.getByUserName(
+				SecurityContextHolder.getContext().getAuthentication().getName()
+		).orElse(null);
+		modelAndView.addObject("boardList", boardService.getAllByUserId(currentUser.getId()));
+		return modelAndView;
+	}
+
+	@PostMapping(path = "/create")
+	public ModelAndView createBoard(@RequestParam String boardTitle) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName(REDIRECT_URL_TO_WELCOME_VIEW);
+		UserDto currentUser = userService.getByUserName(
+				SecurityContextHolder.getContext().getAuthentication().getName()
+		).orElse(null);
+		try {
+			boardHandler.createBoardByDefaultUserIdAndByTitle(currentUser.getId(), boardTitle);
+		} catch (NonExistentUserIdException e) {
+			//TODO kezeling it down or something
+			modelAndView.addObject("nonExistentUserIdError", true);
+		}
+		return modelAndView;
 	}
 }
