664adf2990df855a068eba0a95e8b3705eb99921
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-11-26 12:34:02 +0100

    Added BoardService to getAllByUserId method

--- service/src/main/java/hu/unideb/inf/rft/neuban/service/interfaces/BoardService.java
+++ service/src/main/java/hu/unideb/inf/rft/neuban/service/interfaces/BoardService.java
@@ -3,11 +3,14 @@ package hu.unideb.inf.rft.neuban.service.interfaces;
 import hu.unideb.inf.rft.neuban.service.domain.BoardDto;
 import hu.unideb.inf.rft.neuban.service.exceptions.BoardNotFoundException;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardService {
 
     Optional<BoardDto> get(Long boardId);
+
+    List<BoardDto> getAllByUserId(Long userId);
 
     Long update(BoardDto boardDto) throws BoardNotFoundException;
 }
