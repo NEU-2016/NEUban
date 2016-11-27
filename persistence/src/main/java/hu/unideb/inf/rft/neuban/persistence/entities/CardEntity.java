5a99823a8cc7872be7e99efd73f6f61996e18838
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-11-27 17:50:02 +0100

    Added JoinTable for Card

--- persistence/src/main/java/hu/unideb/inf/rft/neuban/persistence/entities/CardEntity.java
+++ persistence/src/main/java/hu/unideb/inf/rft/neuban/persistence/entities/CardEntity.java
@@ -2,12 +2,10 @@ package hu.unideb.inf.rft.neuban.persistence.entities;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -28,10 +26,17 @@ public class CardEntity extends SuperEntity<Long> {
     @Column(name = "description")
     private String description;
 
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_card_relation_table",
+            joinColumns = @JoinColumn(name = "card_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
+    private List<UserEntity> users;
+
     @Builder
-    public CardEntity(Long id, String title, String description) {
+    public CardEntity(Long id, String title, String description, List<UserEntity> users) {
         super(id);
         this.title = title;
         this.description = description;
+        this.users = users;
     }
 }
