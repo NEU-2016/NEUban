c608a85d490b3fdc24cf3e770466361c0fa9e1c6
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-12-05 00:02:56 +0100

    Bean provides classes updated

---
+++ service/src/main/java/hu/unideb/inf/rft/neuban/service/provider/beanname/SingleDataUpdateServiceBeanNameProvider.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.rft.neuban.service.provider.beanname;
+
+
+public class SingleDataUpdateServiceBeanNameProvider {
+    private SingleDataUpdateServiceBeanNameProvider() {}
+
+    public static final String SINGLE_USER_DATA_UPDATE_SERVICE = "singleUserDataUpdateService";
+    public static final String SINGLE_BOARD_DATA_UPDATE_SERVICE = "singleBoardDataUpdateService";
+    public static final String SINGLE_COLUMN_DATA_UPDATE_SERVICE = "singleColumnDataUpdateService";
+    public static final String SINGLE_CARD_DATA_UPDATE_SERVICE = "singleCardDataUpdateService";
+}
