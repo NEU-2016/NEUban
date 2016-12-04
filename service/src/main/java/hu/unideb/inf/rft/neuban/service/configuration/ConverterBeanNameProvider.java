40e08fc4e05775be9565cb337f5890490eff7e7b
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-12-04 15:47:03 +0100

    Introduce Provider bean name classes

---
+++ service/src/main/java/hu/unideb/inf/rft/neuban/service/configuration/ConverterBeanNameProvider.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.rft.neuban.service.configuration;
+
+
+public class ConverterBeanNameProvider {
+    private ConverterBeanNameProvider() {}
+
+    public static final String SINGLE_USER_DATA_CONVERTER = "singleUserDataConverter";
+    public static final String SINGLE_BOARD_DATA_CONVERTER = "singleBoardDataConverter";
+    public static final String SINGLE_COLUMN_DATA_CONVERTER = "singleColumnDataConverter";
+    public static final String SINGLE_CARD_DATA_CONVERTER = "singleCardDataConverter";
+}
---
+++ service/src/main/java/hu/unideb/inf/rft/neuban/service/configuration/CrudServiceBeanNameProvider.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.rft.neuban.service.configuration;
+
+
+public class CrudServiceBeanNameProvider {
+    private CrudServiceBeanNameProvider() {}
+
+    public static final String SINGLE_USER_DATA_GET_SERVICE = "singleUserDataGetService";
+    public static final String SINGLE_BOARD_DATA_GET_SERVICE = "singleBoardDataGetService";
+    public static final String SINGLE_COLUMN_DATA_GET_SERVICE = "singleColumnDataGetService";
+    public static final String SINGLE_CARD_DATA_GET_SERVICE = "singleCardDataGetService";
+}
