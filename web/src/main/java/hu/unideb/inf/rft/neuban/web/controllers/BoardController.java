3c99eefeccf9d0eaf8b138b511f0c3d170b65cc5
Fekete Attila <atkavagyok@gmail.com>
2016-12-11 13:04:04 +0100

    Board controller BoardNotFoundException throwing is now more compact (orElseThrow used instead of orElse)

--- web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/BoardController.java
+++ web/src/main/java/hu/unideb/inf/rft/neuban/web/controllers/BoardController.java
@@ -32,10 +32,7 @@ public class BoardController {
 	@GetMapping
 	public ModelAndView loadBoardView(@PathVariable final Long boardId) throws BoardNotFoundException {
 		final ModelAndView modelAndView = new ModelAndView(BOARD_VIEW);
-		BoardDto boardModelObject = boardService.get(boardId).orElse(null);
-		if (boardModelObject == null) {
-			throw new BoardNotFoundException(String.valueOf(boardId));
-		}
+		BoardDto boardModelObject = boardService.get(boardId).orElseThrow(() -> new BoardNotFoundException(String.valueOf(boardId)));
 		modelAndView.addObject(BOARD_MODEL_OBJECT_NAME, boardModelObject);
 		return modelAndView;
 	}
