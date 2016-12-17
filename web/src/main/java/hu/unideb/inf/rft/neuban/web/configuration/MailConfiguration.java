7328bb84cac6f7ce883973964df81124ad51ce08
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2016-12-17 17:01:46 +0100

    Introduce mail configuration in web-layer

---
+++ web/src/main/java/hu/unideb/inf/rft/neuban/web/configuration/MailConfiguration.java
@@ -0,0 +1,55 @@
+package hu.unideb.inf.rft.neuban.web.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:mail.properties")
+public class MailConfiguration {
+
+    @Value("${mail.protocol}")
+    private String mailProtocol;
+
+    @Value("${mail.smtp.host}")
+    private String host;
+
+    @Value("${mail.smtp.port}")
+    private Integer port;
+
+    @Value("${mail.support.username}")
+    private String username;
+
+    @Value("${mail.support.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        final JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setProtocol(mailProtocol);
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        final Properties properties = new Properties();
+
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "false");
+        
+        return properties;
+    }
+}
