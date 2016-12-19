f29d292b06a562d42ca0ebd9bdf844c521bbf582
Fekete Attila <atkavagyok@gmail.com>
2016-12-19 19:19:35 +0100

    welcome controller loadWelcomeViewShouldRenderErrorViewIfPrincipalDoesNotExists

--- web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/WelcomeControllerTest.java
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/WelcomeControllerTest.java
@@ -14,7 +14,6 @@ import java.security.Principal;
 import java.util.Optional;
 
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,6 +30,8 @@ public class WelcomeControllerTest extends AbstractControllerTest {
 
 	private static final String EXISTING_PRINCIPAL_USERNAME = "admin";
 	private static final String NON_EXISTING_PRINCIPAL_USERNAME = "admin_but_this_time_it_does_not_exist_haha";
+
+	private static final String EXPECTED_PRINCIPAL_ERROR_MESSAGE_MODEL_OBJECT = "NonExistentPrincipalUserException: Non-existent logged in user :" + NON_EXISTING_PRINCIPAL_USERNAME;
 
 	@InjectMocks
 	private WelcomeController welcomeController;
@@ -52,7 +53,7 @@ public class WelcomeControllerTest extends AbstractControllerTest {
 	@Test
 	public void loadWelcomeViewShouldRenderWelcomeViewIfPrincipalExists() throws Exception {
 		when(principal.getName()).thenReturn(EXISTING_PRINCIPAL_USERNAME);
-		when(userService.getByUserName(anyString())).thenReturn(Optional.of(UserDto.builder().id(1L).userName(EXISTING_PRINCIPAL_USERNAME).build()));
+		when(userService.getByUserName(EXISTING_PRINCIPAL_USERNAME)).thenReturn(Optional.of(UserDto.builder().id(1L).userName(EXISTING_PRINCIPAL_USERNAME).build()));
 		when(boardService.getAllByUserId(anyLong())).thenReturn(Lists.newArrayList());
 		this.mockMvc.perform(get(REQUEST_URL).principal(principal))
 				.andExpect(status().isOk())
@@ -61,5 +62,15 @@ public class WelcomeControllerTest extends AbstractControllerTest {
 				.andExpect(model().attribute(USERNAME_MODEL_OBJECT_NAME, EXISTING_PRINCIPAL_USERNAME))
 				.andExpect(model().attribute(BOARD_LIST_MODEL_OBJECT_NAME, Lists.newArrayList()))
 				.andExpect(model().attribute(CARD_LIST_MODEL_OBJECT_NAME, Lists.newArrayList()));
+	}
+
+	@Test
+	public void loadWelcomeViewShouldRenderErrorViewIfPrincipalDoesNotExists() throws Exception {
+		when(principal.getName()).thenReturn(NON_EXISTING_PRINCIPAL_USERNAME);
+		when(userService.getByUserName(NON_EXISTING_PRINCIPAL_USERNAME)).thenReturn(Optional.empty());
+		this.mockMvc.perform(get(REQUEST_URL).principal(principal))
+				.andExpect(status().isOk())
+				.andExpect(view().name(ERROR_VIEW))
+				.andExpect(model().attribute(ERROR_MESSAGE_MODEL_OBJECT_NAME, EXPECTED_PRINCIPAL_ERROR_MESSAGE_MODEL_OBJECT));
 	}
 }
