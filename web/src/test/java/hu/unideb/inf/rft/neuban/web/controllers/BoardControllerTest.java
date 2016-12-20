6669f776306ed18b2312eeb31c1a04a134e5da1b
Fekete Attila <atkavagyok@gmail.com>
2016-12-20 01:45:23 +0100

    BoardController addCardShouldRenderBoardViewIfBoardIdAndColumnIdAndCardTitleAreValid

--- web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/BoardControllerTest.java
+++ web/src/test/java/hu/unideb/inf/rft/neuban/web/controllers/BoardControllerTest.java
@@ -1,9 +1,11 @@
 package hu.unideb.inf.rft.neuban.web.controllers;
 
 import hu.unideb.inf.rft.neuban.service.domain.BoardDto;
+import hu.unideb.inf.rft.neuban.service.domain.CardDto;
 import hu.unideb.inf.rft.neuban.service.domain.ColumnDto;
 import hu.unideb.inf.rft.neuban.service.domain.UserDto;
 import hu.unideb.inf.rft.neuban.service.interfaces.BoardService;
+import hu.unideb.inf.rft.neuban.service.interfaces.CardService;
 import hu.unideb.inf.rft.neuban.service.interfaces.ColumnService;
 import hu.unideb.inf.rft.neuban.service.interfaces.UserService;
 import org.junit.Test;
@@ -17,9 +19,7 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -32,6 +32,7 @@ public class BoardControllerTest extends AbstractControllerTest {
 	private static final Long INVALID_BOARD_ID = 667L;
 	private static final Long VALID_COLUMN_ID = 1L;
 	private static final String VALID_COLUMN_TITLE = "Valid column title";
+	private static final String VALID_CARD_TITLE = "Valid card title";
 
 	private static final String BOARD_URL = "/secure/board";
 	private static final String VALID_BOARD_URL = BOARD_URL + "/" + String.valueOf(VALID_BOARD_ID);
@@ -39,6 +40,7 @@ public class BoardControllerTest extends AbstractControllerTest {
 
 	private static final String CREATE_COLUMN_URL = BOARD_URL + "/" + String.valueOf(VALID_BOARD_ID) + "/createcolumn";
 	private static final String REMOVE_COLUMN_URL = BOARD_URL + "/" + String.valueOf(VALID_BOARD_ID) + "/removecolumn/" + String.valueOf(VALID_COLUMN_ID);
+	private static final String ADD_CARD_URL = BOARD_URL + "/" + String.valueOf(VALID_BOARD_ID) + "/" + String.valueOf(VALID_COLUMN_ID) + "/addcard";
 
 	private static final String BOARD_VIEW = "secure/board";
 	private static final String REDIRECT_TO_BOARD_VIEW = "redirect:/" + BOARD_VIEW + "/" + String.valueOf(VALID_BOARD_ID);
@@ -46,6 +48,7 @@ public class BoardControllerTest extends AbstractControllerTest {
 	private static final String BOARD_ID_REQUEST_PARAM_NAME = "boardId";
 	private static final String COLUMN_ID_REQUEST_PARAM_NAME = "columnId";
 	private static final String COLUMN_TITLE_REQUEST_PARAM_NAME = "columnTitle";
+	private static final String CARD_TITLE_REQUEST_PARAM_NAME = "cardTitle";
 
 	private static final String USERNAME_MODEL_OBJECT_NAME = "username";
 	private static final String BOARD_MODEL_OBJECT_NAME = "board";
@@ -67,6 +70,9 @@ public class BoardControllerTest extends AbstractControllerTest {
 
 	@Mock
 	private ColumnService columnService;
+
+	@Mock
+	private CardService cardService;
 
 	@Override
 	protected Object[] controllerUnderTest() {
@@ -136,12 +142,26 @@ public class BoardControllerTest extends AbstractControllerTest {
 	// TODO Invalid tests
 
 	@Test
-	public void removeColumnShouldRenderBoardViewIfBoardIdAndColumnIdAndCardTitleAreValid() throws Exception {
+	public void removeColumnShouldRenderBoardViewIfBoardIdAndColumnIdAreValid() throws Exception {
 		doNothing().when(columnService).remove(VALID_COLUMN_ID);
 		this.mockMvc.perform(
 				delete(REMOVE_COLUMN_URL)
 						.param(BOARD_ID_REQUEST_PARAM_NAME, String.valueOf(VALID_BOARD_ID))
 						.param(COLUMN_ID_REQUEST_PARAM_NAME, String.valueOf(VALID_COLUMN_ID)))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(view().name(REDIRECT_TO_BOARD_VIEW));
+	}
+
+	// TODO Invalid tests
+
+	@Test
+	public void addCardShouldRenderBoardViewIfBoardIdAndColumnIdAndCardTitleAreValid() throws Exception {
+		doNothing().when(cardService).save(VALID_COLUMN_ID, CardDto.builder().title(VALID_CARD_TITLE).build());
+		this.mockMvc.perform(
+				post(ADD_CARD_URL)
+						.param(BOARD_ID_REQUEST_PARAM_NAME, String.valueOf(VALID_BOARD_ID))
+						.param(COLUMN_TITLE_REQUEST_PARAM_NAME, VALID_COLUMN_TITLE)
+						.param(CARD_TITLE_REQUEST_PARAM_NAME, VALID_CARD_TITLE))
 				.andExpect(status().is3xxRedirection())
 				.andExpect(view().name(REDIRECT_TO_BOARD_VIEW));
 	}
